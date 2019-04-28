98ddc0136b7d987e924158fc8289845e3fa63c85
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-04-28 02:58:10 +0200

    basic collision resolution

--- Assets/Scripts/WheelchairMoveScript.cs
+++ Assets/Scripts/WheelchairMoveScript.cs
@@ -53,6 +53,8 @@ public class WheelchairMoveScript : MonoBehaviour {
 	public float CameraTurnSpeedScale = 1.0f;
 	public float CameraTurnDeadZone = 1.0f;
 
+	public bool EnableCollision = false;
+
 	void Start() {
 		//DriftTimer = new Timer(DriftDuration);
 		//DriftTimer.Stop();
@@ -295,14 +297,17 @@ public class WheelchairMoveScript : MonoBehaviour {
 
 	}
 
-	/*
-	private void OnCollisionEnter(Collision other) {
-		
-		// Turn 90 degrees when hitting a wall
-		// TODO: find which way to turn
-        transform.Rotate(Vector3.up, 90);
-		
+	private void OnTriggerEnter(Collider other) {
+
+		if (!EnableCollision)
+		{
+			return;
+		}
+
+		Debug.Log("hit wall!");
+		// Turn 180 degrees when hitting a wall
+		// TODO: turn 90 (or 135?) degrees left or right depending on which direction wall was hit
+        transform.Rotate(Vector3.up, 180);
 	}
-	//*/
 
 }
