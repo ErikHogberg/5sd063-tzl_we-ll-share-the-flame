c0d3d7fcc22df4dea23e0c6f7af2a0478c1ddf60
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-05-10 17:07:47 +0200

    remove psmove stuff, add cinematic script

---
+++ Assets/Scripts/EriksCinematicScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CameraSpot {
+	public GameObject AnchorObject;
+	public float StartYaw;
+	public float StartPitch;
+	public float Speed;
+	public float Time;
+}
+
+public class EriksCinematicScript : MonoBehaviour {
+
+	// [SerializeField]
+	public List<CameraSpot> CameraSpots;
+
+	void Start() {		
+
+	}
+
+	void Update() {
+
+	}
+}
