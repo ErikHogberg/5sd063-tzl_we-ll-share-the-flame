3016ab37b905444128dcae6b786b3fb40c481c48
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-05-13 17:44:42 +0200

    graffiti removal attempt

---
+++ Assets/Scripts/GraffitiScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraffitiScript : MonoBehaviour {
+
+	public float removalSpeed = 0.01f;
+	public float alpha = 1f;
+
+	
+
+	void Start() {
+
+	}
+
+	void OnParticleCollision(GameObject other) {
+		alpha -= removalSpeed;
+		// TODO: set alpha
+		// gameObject.SetActive(false);
+	}
+
+	// void OnParticleTrigger() {
+	//     Debug.Log("particle trigger");
+
+	// }
+
+}
