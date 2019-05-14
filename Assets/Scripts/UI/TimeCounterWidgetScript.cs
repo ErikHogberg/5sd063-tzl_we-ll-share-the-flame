fc2bc947bca2e7b276a866dc69dcdef760a0ebeb
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-05-15 01:17:49 +0200

    Timer and score counter widgets

---
+++ Assets/Scripts/UI/TimeCounterWidgetScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Utilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounterWidgetScript : MonoBehaviour {
+
+	[Tooltip("time in seconds")]
+	public float time;
+	private Timer timer;
+
+	private Text text;
+
+	void Start() {
+
+		text = GetComponent<Text>();
+
+		timer = new Timer(time);
+
+	}
+
+
+	void Update() {
+
+		int timeLeft = Mathf.FloorToInt(timer.TimeLeft() * 100);
+
+		int minutes = timeLeft / 6000;
+		int seconds = (timeLeft % 6000) / 100;
+		int tensOfMilliseconds = (timeLeft % 6000) % 100;
+
+        text.text = "" + minutes + ":" + seconds.ToString("00") + ":" + tensOfMilliseconds.ToString("00");
+
+		if (timer.Update()) {
+			// TODO: do something when time runs out, change scene? trigger game over?
+		}
+	}
+}
