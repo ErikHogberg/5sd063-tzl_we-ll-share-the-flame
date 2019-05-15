f15780183942d4a8ed9a095e71e98f6cd5cb32fb
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-05-16 00:52:31 +0200

    score multiplier timer bar

--- Assets/Scripts/UI/ScoreMultiplierWidgetScript.cs
+++ Assets/Scripts/UI/ScoreMultiplierWidgetScript.cs
@@ -12,7 +12,9 @@ public class ScoreMultiplierWidgetScript : MonoBehaviour {
 
 	public Text MultiplierText;
 	public Text NotificationText;
+	public Image TimerBar;
 	public float NotificationTime;
+	public AnimationCurve TimerCurve;
 
 	private class ScoreNotification {
 		public Timer Timer;
@@ -34,12 +36,17 @@ public class ScoreMultiplierWidgetScript : MonoBehaviour {
 	void Update() {
 		string outText = "";
 
+		float timerBarProgress = 0f;
+
 		// TODO: limit number of lines shown, only show newest
 		notifications.RemoveAll((n) => {
 			if (n.Timer.Update()) {
 				return true;
 			} else {
 				outText += n.Text;
+				if (timerBarProgress < n.Timer.TimeLeft()) {
+					timerBarProgress = n.Timer.TimeLeft();
+				}
 				return false;
 			}
 		});
@@ -48,13 +55,18 @@ public class ScoreMultiplierWidgetScript : MonoBehaviour {
 			Globals.ScoreMultiplier = 1f;
 		}
 
+		Vector3 scale = TimerBar.transform.localScale;
+		scale.x = TimerCurve.Evaluate(timerBarProgress / NotificationTime);
+		TimerBar.transform.localScale = scale;
+
 		NotificationText.text = outText;
 		MultiplierText.text = Globals.ScoreMultiplier.ToString();
 	}
 
 	public float AddPoints(float points) {
 		notifications.Add(new ScoreNotification(
-			NotificationTime, "+" + Globals.ScoreMultiplier.ToString("F1")
+			NotificationTime, 
+			"+" + Globals.ScoreMultiplier.ToString("F1")
 			+ "x" + points.ToString("F0") + Environment.NewLine
 			));
 		return points * Globals.ScoreMultiplier;
