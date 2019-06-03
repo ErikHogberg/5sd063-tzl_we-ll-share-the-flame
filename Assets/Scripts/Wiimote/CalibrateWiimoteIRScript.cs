d94b2216cd2522b35184f673593a79ca79efa1df
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-06-03 20:27:35 +0200

    mitigate foam lag, calibrate wiimote motion plus with 7 key

--- Assets/Scripts/Wiimote/CalibrateWiimoteIRScript.cs
+++ Assets/Scripts/Wiimote/CalibrateWiimoteIRScript.cs
@@ -9,6 +9,13 @@ public class CalibrateWiimoteIRScript : MonoBehaviour {
 	void Update() {
 		Keyboard keyboard = Keyboard.current;
 		if (Input.GetKeyDown("8")) {
+			if (WiimoteManager.HasWiimote()) {
+				Wiimote wiimote = WiimoteManager.Wiimotes[0];
+				wiimote.MotionPlus.SetZeroValues();
+			}
+			Debug.Log("Calibrated Motion Plus");
+		}
+		if (Input.GetKeyDown("8")) {
 			Globals.irToggle = false;
 			Debug.Log("Disabled IR");
 		}
